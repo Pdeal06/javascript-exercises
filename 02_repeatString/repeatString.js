bc463644b5584153661ef6268dd542f53769300c
Phil <pdeal06@gmail.com>
2022-04-15 09:40:26 -0500

    Make function that passes a few tests on 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let output = ''
+  for (let i = 0; i < num; i++) {
+    if (i == num - 1){
+      output += string;
+      return output;
+    } else {
+      output += string
+      continue
+    }
+     
+  }
+  
 };
 
 // Do not edit below this line
