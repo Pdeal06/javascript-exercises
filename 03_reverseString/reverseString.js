e81b7afbc947389442e6e8c497f23820af8a91e8
Phil <pdeal06@gmail.com>
2022-04-16 19:28:50 -0500

    Pass all tests on 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let strLength = string.length;
+  let newString = '';
+  for (let i = 1; i <= strLength; i++){
+    newString += string.substr(-i,1);
+  }
+  return newString
 };
 
 // Do not edit below this line
