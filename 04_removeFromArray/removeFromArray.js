48fa8c5aa7f5d63f9311c5e69a7a966b5ac0ef72
Phil <pdeal06@gmail.com>
2022-04-19 09:33:10 -0500

    Attempt of function for 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
 const removeFromArray = function() {
+  let array = arguments[0];
+  const finalArray = [];
+  for (let i = 1; i < arguments.length; i++){
+    for (const arrayValue of array){
+      if (arrayValue === arguments[i]) {
+        continue;
+      } else {
+        finalArray.push(arguments[i]);
+      }
+    }
+  }
+  return finalArray
+
 
 };
 
