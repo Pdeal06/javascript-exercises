bfd5401de19078bd68370b4997f9dd25f569b8d7
Phil <pdeal06@gmail.com>
2022-04-20 21:24:15 -0500

    Starts new version

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,23 +1,5 @@
-const removeFromArray = function() {
-  let array = arguments[0];
-  let filteredArray = [];
-  for (let i = 1; i < arguments.length; i++){
-  if (i === arguments.length - 1 && arguments.length > 2) {
-    filteredArray = filteredArray.filter(item => item !== arguments[i])
-    return filteredArray; 
-  }else if (i == 1 && arguments.length == 2  ) {
-    filteredArray = array.filter(item => item !== arguments[i])
-    return filteredArray
-  } else if (i === arguments.length - 1) {
-    filteredArray = filteredArray.filter(item => item !== arguments[i])
-    return filteredArray
+const removeFromArray = function(array, ...theUnwanted) {
 
-    } else if (i == 1) {  
-      filteredArray = array.filter(item => item !== arguments[i])
-    } else {filteredArray = array.filter(item => item !== arguments[i])
-    }
-  }
 }
-
 // Do not edit below this line
 module.exports = removeFromArray;
