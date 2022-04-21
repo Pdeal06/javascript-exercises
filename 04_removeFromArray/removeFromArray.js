3b355ae389325b8c5bafb30cbfa683d3bf7ba092
Phil <pdeal06@gmail.com>
2022-04-20 20:57:59 -0500

    Works for some of 04 not all

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,19 +1,23 @@
 const removeFromArray = function() {
   let array = arguments[0];
-  const finalArray = [];
+  let filteredArray = [];
   for (let i = 1; i < arguments.length; i++){
-    for (const arrayValue of array){
-      if (arrayValue === arguments[i]) {
-        continue;
-      } else {
-        finalArray.push(arguments[i]);
-      }
+  if (i === arguments.length - 1 && arguments.length > 2) {
+    filteredArray = filteredArray.filter(item => item !== arguments[i])
+    return filteredArray; 
+  }else if (i == 1 && arguments.length == 2  ) {
+    filteredArray = array.filter(item => item !== arguments[i])
+    return filteredArray
+  } else if (i === arguments.length - 1) {
+    filteredArray = filteredArray.filter(item => item !== arguments[i])
+    return filteredArray
+
+    } else if (i == 1) {  
+      filteredArray = array.filter(item => item !== arguments[i])
+    } else {filteredArray = array.filter(item => item !== arguments[i])
     }
   }
-  return finalArray
-
-
-};
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
