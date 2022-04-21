4007b1f4ab7b49e470e6284c3d88fcc8dd81f8da
Phil <pdeal06@gmail.com>
2022-04-21 09:15:10 -0500

    Finish 04. I love how it turned out

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
 const removeFromArray = function(array, ...theUnwanted) {
-
+  function removeUnwanted(value) {
+    let answer = theUnwanted.includes(value);
+    return !answer
+  }
+  
+  let filtered = array.filter(removeUnwanted);
+  return filtered
 }
 // Do not edit below this line
 module.exports = removeFromArray;
